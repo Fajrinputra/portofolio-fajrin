82501467351d029e255ff2320049f631f2319b0a
Fajrin Putra Pratama <fajrinputrapratama01@gmail.com>
2026-08-28 11:59:33 +0700

    style: remove text from logo and recenter shape

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -18,7 +18,7 @@ export default function Logo({ size = 36, className = '' }) {
       <rect width="100" height="100" rx="22" fill="url(#jejeGradBg)" />
 
       {/* Shape S/Infinity yang dimiringkan */}
-      <g transform="translate(50, 42) scale(0.68) rotate(-32) translate(-50, -45)">
+      <g transform="translate(50, 50) scale(0.75) rotate(-32) translate(-50, -45)">
         <path
           d="M 50 73 L 25 73 A 14 14 0 0 1 25 45 L 75 45 A 14 14 0 0 0 75 17 L 50 17"
           stroke="white"
@@ -27,20 +27,6 @@ export default function Logo({ size = 36, className = '' }) {
           fill="none"
         />
       </g>
-
-      {/* Text JEJE di bawah logo */}
-      <text
-        x="50"
-        y="90"
-        textAnchor="middle"
-        fill="white"
-        fontSize="17"
-        fontWeight="800"
-        fontFamily="system-ui, -apple-system, sans-serif"
-        letterSpacing="2"
-      >
-        JEJE
-      </text>
 
       <defs>
         <linearGradient id="jejeGradBg" x1="0" y1="0" x2="100" y2="100" gradientUnits="userSpaceOnUse">
